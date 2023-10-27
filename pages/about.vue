f4ab7074becb5a4f3b64d41bbec0a6f60ba29682
t.tsukamoto <tmak.tsukamoto@gmail.com>
2023-10-27 10:22:48 +0900

    はじめてのpagesルーティング

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-amber-100">
+    <h1 class="text-3xl mb-3">About</h1>
+    <p>help</p>
+    <ULink to="/">Home</ULink>
+    <UButton to="/">Home</UButton>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'My home page'
+})
+</script>
+
+<template>
+  <div class="bg-violet-100">
+    <h1 class="text-3xl mb-3">Home</h1>
+    <p>message</p>
+    <!--- link to about -->
+    <ULink to="/about">About</ULink>
+    <UButton to="/about">About</UButton>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
