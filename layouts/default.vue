34875484d41b607ef6e0f3bb19261085879fbe71
t.tsukamoto <tmak.tsukamoto@gmail.com>
2023-10-27 10:58:59 +0900

    はじめてのLayout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UContainer class="min-h-screen bg-yellow-50 box-border border border-dashed border-yellow-700">
+    <div>
+      <ULink to="/" class="underline mr-2">MyApp</ULink>
+      <ULink to="/about" class="underline mr-2">About</ULink>
+    </div>
+    <slot/>
+  </UContainer>
+</template>
+
+<style scoped>
+
+</style>
